bfa6c52673886a30651d3994522c17580b65fbad
hoffs <stephanie.nhoffman7@gmail.com>
2020-05-16 21:13:56 -0700

    Adding solution to adjacent element product problem

---
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -0,0 +1,14 @@
+function adjacentElementsProduct(inputArray) {
+    let largestProduct = Number.MAX_VALUE * -1;
+
+    for (var i = 0; i < inputArray.length - 1; i++) {
+        var j = i + 1;
+        var product = inputArray[i] * inputArray[j];
+
+        if (product > largestProduct) {
+            largestProduct= product;
+        }
+    }
+    
+    return largestProduct;
+}
