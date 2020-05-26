48483fa51bd865c6783cc8616d26efca7757fcad
hoffs <stephanie.nhoffman7@gmail.com>
2020-05-26 00:28:59 -0700

    push of initial solution. Working to fix and update

--- makeArrayConsecutive/makeArrayConsecutive.js
+++ makeArrayConsecutive/makeArrayConsecutive.js
@@ -1,3 +1,17 @@
 function makeArrayConsecutive2(statues) {
-    
+    let numberArray = [];
+    // Order numbers from smallest to greatest
+
+    // set count = 0; 
+    var j = i +1;
+
+    for (var i = 0; i < Array.length; i ++) {
+        var diffCheck = j - i;
+        
+        if (diffCheck != 1) {
+            count = count + diffCheck - 1; 
+        }
+    }
+
+    return count;
 }
