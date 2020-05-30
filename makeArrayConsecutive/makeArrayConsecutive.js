51af4e4d1676dc1fd11434f11b2b269efb8ca05f
hoffs <stephanie.nhoffman7@gmail.com>
2020-05-29 22:44:07 -0700

    updated variable names

--- makeArrayConsecutive/makeArrayConsecutive.js
+++ makeArrayConsecutive/makeArrayConsecutive.js
@@ -1,12 +1,12 @@
 function makeArrayConsecutive2(statues) {
     let numberArray = [];
-    numberArray = numberArray.sort()
+    numberArray = numberArray.sort();
 
-    var count = 0; 
-    var j = i +1;
+    var count = 0;
+    var j = i + 1;
 
-    for (var i = 0; i < Array.length; i ++) {
-        var diffCheck = j - i;
+    for (var i = 0; i < numberArray.length; i ++) {
+        var diffCheck = j - i - 1;
         
         if (diffCheck != 1) {
             count = count + diffCheck - 1; 
