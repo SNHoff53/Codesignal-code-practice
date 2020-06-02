ae5446a548d5181796702d3eccd9a748fcf19578
hoffs <stephanie.nhoffman7@gmail.com>
2020-06-01 23:26:59 -0700

    adding in case to account for some edge cases. Looking at three numbers within the array to determine value instead of two numbers. Working to determine how to solve for deciding to remove a number in order to make sequence increasing for those TRUE cases

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,11 +1,21 @@
 function almostIncreasingSequence(sequence) {
     // initializing the countDecrease to zero
    var countDecrease = 0;
-   for (var firstNum = 0; firstNum < sequence.length - 1; firstNum++) {
-       var secondNum = firstNum + 1;
-       if (sequence[firstNum] >= sequence[secondNum]) {
+   for (var focusNum = 1; focusNum < sequence.length - 1; focusNum++) {
+       var leftNum = focusNum - 1;
+       var rightNum = focusNum + 1;
+       console.log("focusNum >= rightNum = " + focusNum >= rightNum)
+       if (sequence[focusNum] >= sequence[rightNum]) {
            countDecrease++;
+           if (sequence[leftNum] >= sequence[rightNum]) {
+               countDecrease++;
+           }
        }
+       // if (sequence[focusNum] <= sequence[leftNum] && sequence[focusNum] <= sequence[rightNum]){
+       //     // focus is bod
+       //     countDecrease++;
+       // } 
+       console.log(countDecrease)
    }
    return countDecrease < 2;
 }
