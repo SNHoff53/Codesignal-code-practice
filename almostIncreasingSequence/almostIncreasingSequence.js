cf17e4ae3507024ae5fa234d060af1df079cab5e
hoffs <stephanie.nhoffman7@gmail.com>
2020-07-16 21:26:44 -0700

    adding comments to code for better read

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,8 +1,11 @@
 function almostIncreasingSequence(sequence) {
     // initializing the countDecrease to zero
    var countDecrease = 0;
+   // iterating through the nunmber sequence
    for (var focusNum = 1; focusNum < sequence.length - 1; focusNum++) {
+       // checking the number to the left of the focusNum and assigning it to a variable
        var leftNum = focusNum - 1;
+       // checking the number to the right of the focusNum and assigning it to a variable
        var rightNum = focusNum + 1;
        console.log("focusNum >= rightNum = " + focusNum >= rightNum)
        if (sequence[focusNum] >= sequence[rightNum]) {
