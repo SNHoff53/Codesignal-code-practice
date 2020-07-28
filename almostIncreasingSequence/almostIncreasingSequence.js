602515d1f5c98b971d5b15bbb75bfa2268bdfa03
hoffs <stephanie.nhoffman7@gmail.com>
2020-07-27 23:57:11 -0700

    updating code -- code reflects case in which focusNum and rightNum are the same, then remove the rightNum

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,25 +1,26 @@
 function almostIncreasingSequence(sequence) {
     
     // initializing the countDecrease to zero
+    // initializing whether the duplicate number, if any, has been removed; setting it to false
    var countDecrease = 0;
+   var haveRemoved = false;
    
    // iterating through the number sequence checking to see if the rightNum is less than the focusNum 
    for (var focusNum = 0; focusNum < sequence.length - 1; focusNum++) {
        var rightNum = focusNum + 1;
        if (sequence[focusNum] >= sequence[rightNum]) {
-           sequence.splice(focusNum, 1);
+           // if the focusNum of the sequence is the same as the rightNum of the sequence, then remove the rightNum
+           if (sequence[focusNum -1] == sequence[rightNum]) {
+               sequence.splice(rightNum, 1);
+               haveRemoved = true;
+           } else {
+               if (focusNum == sequence.length - 2) {
+                   sequence.pop();
+               }
+               sequence.splice(focusNum, 1);
+               haveRemoved = true;
+           }
            break;
        }
    }
-   
-   for (var index = 0; index < sequence.length - 1; index++) {
-       var rightNum = index + 1;
-       if (sequence[index] >= sequence[rightNum]) {
-           console.log('index broke me: ' + index)
-           return false;
-       }
-       console.log('fine')
-   }
-   console.log(sequence)
-   return  true;
-}
+};
